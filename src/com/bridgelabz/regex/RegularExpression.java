2e79aa305ed3cb16855bb33bcc05bed7cf0f4f9d
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-02-09 00:24:18 +0530

    Added: Validate password Rule 1

--- src/com/bridgelabz/regex/RegularExpression.java
+++ src/com/bridgelabz/regex/RegularExpression.java
@@ -6,10 +6,13 @@ import java.util.regex.Pattern;
 
 public class RegularExpression {
 
-    static Pattern pattern = Pattern.compile("^(^([0-9]{2})\s([0-9]{10})$)");
+    static Pattern pattern = Pattern.compile("^([a-z]{8,})$");
     public static void main(String[] args)
     {
-        Matcher matcher = pattern.matcher("91 8999652255");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Password:");
+        String password = s.next();
+        Matcher matcher = pattern.matcher(password);
         if(matcher.matches())
             System.out.println(true);
         else
